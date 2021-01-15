19b48455aee57f3c7cd53bd84a661b1563ef7b26
ft210161vav <a.vinarsky61@gmail.com>
2021-01-15 17:08:16 +0200

    third-commit

--- junit/src/test/java/SampleTest.java
+++ junit/src/test/java/SampleTest.java
@@ -1,40 +1,46 @@
-import org.apache.logging.log4j.LogManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import config.ServerConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.logging.LogManager;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class SampleTest {
-    private Logger logger= LogManager.getLogManager().getLogger(SampleTest.class);
-    private ServerConfig cfg=CongifFactory.create(ServerConfig.class);
-    @Test
-    public void log(){
-    logger.info("Я инфолог");
-}
-}
-@BeforeEach
+    //private final Logger logger = Logger.getLogger(String.valueOf(SampleTest.class));
+    private final Logger logger = LogManager.getLogger("SampleTest.class");
+    final ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+    protected static WebDriver driver;
 
-public void setUp()
-{
-   ChromeDriver driver = new ChromeDriver();
-    logger.info("Драйвер поднят");
-}
-@Test
-public void openPage()
-{
-    driver.get("https://otus.ru");
-    log.info("Открыта страница Otus.ru");
-}
-@AfterEach
-public void setDown()
-{
-    if (driver!=null)
-    {
-        driver.quit();
-        logger.info("Драйвер закрыт");
+    //   public static void initDriver() {
+    //      System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe");
 
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
     }
 
+    @Test
+    public void openPage() {
+        driver.get(cfg.url());
+        logger.info("Открыта страница Otus.ru");
+        String actualTitle = driver.getTitle();
+        logger.info("title: "+actualTitle);
+    }
+
+
+    @After
+    public void setDown() {
+        if (driver != null) {
+            driver.quit();
+            logger.info("Драйвер закрыт");
+        }
+
+
+    }
 }
