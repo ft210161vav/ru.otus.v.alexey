1dddf15dc0c78ddc7c21d390846a31c726d7a9ea
ft210161vav <a.vinarsky61@gmail.com>
2021-01-16 16:58:13 +0200

    'fourth-commit

--- junit/src/test/java/SampleTest.java
+++ junit/src/test/java/SampleTest.java
@@ -1,14 +1,13 @@
 import config.ServerConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 public class SampleTest {
     //private final Logger logger = Logger.getLogger(String.valueOf(SampleTest.class));
     private final Logger logger = LogManager.getLogger("SampleTest.class");
